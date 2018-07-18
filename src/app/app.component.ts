40a58d8271d7b3ff57b470f4238999b317185b62
EL SEIFY Ramy <elseify.ramy@gmail.com>
2018-07-18 14:02:32 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,31 +2,21 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { Auth } from 'aws-amplify';
 
-import { TabsPage } from '../pages/tabs/tabs';
-import { LoginPage } from '../pages/login/login';
-
+import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = null;
+  rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    let globalActions = function() {
+    platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
-    };
-
-    platform.ready()
-      .then(() => {
-        Auth.currentAuthenticatedUser()
-          .then(() => { this.rootPage = TabsPage; })
-          .catch(() => { this.rootPage = LoginPage; })
-          .then(() => globalActions());
-      });
+    });
   }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,16 @@
-import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-
-import { Camera } from '@ionic-native/camera';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { LoginPage } from '../pages/login/login';
-import { SignupPage } from '../pages/signup/signup';
-import { ConfirmSignInPage } from '../pages/confirmSignIn/confirmSignIn';
-import { ConfirmSignUpPage } from '../pages/confirmSignUp/confirmSignUp';
-import { SettingsPage } from '../pages/settings/settings';
-import { AboutPage } from '../pages/about/about';
-import { AccountPage } from '../pages/account/account';
-import { TabsPage } from '../pages/tabs/tabs';
-import { TasksPage } from '../pages/tasks/tasks';
-import { TasksCreatePage } from '../pages/tasks-create/tasks-create';
-
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-
-import { DynamoDB } from '../providers/aws.dynamodb';
-
-import Amplify from 'aws-amplify';
-const aws_exports = require('../aws-exports').default;
-
-Amplify.configure(aws_exports);
+import { HomePage } from '../pages/home/home';
 
 @NgModule({
   declarations: [
     MyApp,
-    LoginPage,
-    SignupPage,
-    ConfirmSignInPage,
-    ConfirmSignUpPage,
-    SettingsPage,
-    AboutPage,
-    AccountPage,
-    TabsPage,
-    TasksPage,
-    TasksCreatePage
+    HomePage
   ],
   imports: [
     BrowserModule,
@@ -47,26 +19,12 @@ Amplify.configure(aws_exports);
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    LoginPage,
-    SignupPage,
-    ConfirmSignInPage,
-    ConfirmSignUpPage,
-    SettingsPage,
-    AboutPage,
-    AccountPage,
-    TabsPage,
-    TasksPage,
-    TasksCreatePage
+    HomePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
-    Camera,
-    DynamoDB
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
 export class AppModule {}
-
-declare var AWS;
-AWS.config.customUserAgent = AWS.config.customUserAgent + ' Ionic';
